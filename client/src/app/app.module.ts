2483d9f1fb3a02e3a239fa1265de9a4060a6e7d7
jeffrey grospe <jeffreygrospe@yahoo.com>
2024-07-30 15:23:30 -0400

    fix directory and module imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './page/home/home.component';
-import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PortfolioComponent } from './page/portfolio/portfolio.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
 
 @NgModule({
