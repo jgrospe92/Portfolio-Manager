114bc102ec6b7f254ac08464d8470b3f6d2e0e73
jeffrey grospe <jeffreygrospe@yahoo.com>
2024-07-30 13:56:50 -0400

    updage angular version to 14.3.0

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { animation } from '@angular/animations';
 import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
+import { PortfolioComponent } from './page/portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
