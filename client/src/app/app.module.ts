ffdc024abbdc695814e9ad63300aabbc7e195be4
Ayman <atmaniayman2@gmail.com>
2024-07-30 15:44:41 -0400

    add communication service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { NavComponent } from './shared/nav/nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import { CommunicationService } from './services/communication.service';
 
 @NgModule({
   declarations: [
@@ -17,9 +19,10 @@ import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
     PageNotFoundComponent,
     NavComponent,
     PortfolioComponent,
+    
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, AgGridAngular],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, AgGridAngular, HttpClientModule],
+  providers: [CommunicationService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
