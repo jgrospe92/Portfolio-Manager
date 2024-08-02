40cde810fe97077053021b2272c9db6605cc07b5
jeffrey grospe <jeffreygrospe@yahoo.com>
2024-08-02 14:09:20 -0400

    change homepage banner & redirect buttons to portfolio page

--- client/src/app/shared/nav/nav.component.ts
+++ client/src/app/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -13,11 +14,11 @@ export class NavComponent implements OnInit {
 
   isMenuCollapsed: boolean = true;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   login() {
-    alert('Not yet implemented');
+    this.router.navigate(['/portfolio']);
   }
 }
